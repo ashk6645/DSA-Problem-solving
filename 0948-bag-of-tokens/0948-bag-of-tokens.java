49f92e6cf71918560d970f283c3d1a435aaba924
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2024-03-04 22:05:46 +0530

    Time: 2 ms (97.13%), Space: 42.4 MB (92.99%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        int low = 0;
+        int high = tokens.length - 1;
+        int score = 0;
+        Arrays.sort(tokens);
+         while (low <= high) {
+            // When we have enough power, play lowest token face-up
+            if (power >= tokens[low]) {
+                score += 1;
+                power -= tokens[low];
+                low += 1;
+            }
+             else if (low < high && score > 0) {
+                score -= 1;
+                power += tokens[high];
+                high -= 1;
+             }
+              else {
+                return score;
+            }
+        }
+        return score;
+        
+    }
+}
