f68371249b4a2fa9a888a17f6a3f3880c6c5cd2a
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2024-03-11 18:23:34 +0530

    Time: 55 ms (97.82%), Space: 49.2 MB (43.44%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,24 +1,29 @@
 class Solution {
     public int countPrimes(int n) {
+        if(n<=2)
+        {
+            return 0;
+        }
+        int count = 1;
         boolean[] prime = new boolean[n];
-        for(int i=2;i<n;i++)
+        for(int i=3;i<n;i+=2)
         {
             prime[i] = true;
         }
-         for (int p = 2; p * p < n; p++) 
+         for (int p = 3; p * p < n; p+=2) 
          {
             // If prime[p] is not changed, then it is a prime
             if (prime[p]) 
             {
                 // Update all multiples of p
-                for (int i = p * p; i < n; i += p)
+                for (int i = p * p; i < n; i += 2*p)
                 {
                     prime[i] = false;
                 }
             }
         }
-        int count = 0;
-        for(int i=2;i<n;i++)
+        
+        for(int i=3;i<n;i+=2)
         {
             if(prime[i])
             {
