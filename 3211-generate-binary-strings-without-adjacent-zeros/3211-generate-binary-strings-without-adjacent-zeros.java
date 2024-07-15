d4c0d35e4e7a216ccbbfd64e5e6d79f9bee2acda
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2024-07-15 09:10:48 +0530

    Time: 2 ms (91.08%), Space: 45.9 MB (27.83%) - LeetHub

--- 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.java
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.java
@@ -2,37 +2,26 @@ class Solution
 {
     public List<String> validStrings(int n)
     {
-        if (n == 0) return new ArrayList<>();
-        
-        List<String> dp0 = new ArrayList<>(); // strings ending with '0'
-        List<String> dp1 = new ArrayList<>(); // strings ending with '1'
-        
-        dp0.add("0");
-        dp1.add("1");
-        
-        for (int i = 2; i <= n; i++) {
-            List<String> newDp0 = new ArrayList<>();
-            List<String> newDp1 = new ArrayList<>();
-            
-            // All strings ending with '1' can have '0' appended
-            for (String s : dp1) {
-                newDp0.add(s + "0");
-            }
-            
-            // All strings can have '1' appended
-            for (String s : dp0) {
-                newDp1.add(s + "1");
-            }
-            for (String s : dp1) {
-                newDp1.add(s + "1");
-            }
-            
-            dp0 = newDp0;
-            dp1 = newDp1;
+        List<String> result = new ArrayList<>();
+        if (n == 0) return result;
+        backtrack(result, new StringBuilder(), n);
+        return result;
+    }
+    
+    private static void backtrack(List<String> result, StringBuilder current, int n) {
+        if (current.length() == n) {
+            result.add(current.toString());
+            return;
         }
         
-        // Merge results from both dp0 and dp1
-        dp1.addAll(dp0);
-        return dp1;
+        if (current.length() == 0 || current.charAt(current.length() - 1) == '1') {
+            current.append('0');
+            backtrack(result, current, n);
+            current.deleteCharAt(current.length() - 1);
+        }
+        
+        current.append('1');
+        backtrack(result, current, n);
+        current.deleteCharAt(current.length() - 1);
     }
 }
