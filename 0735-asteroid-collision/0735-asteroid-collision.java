a8a7d4c4c29b6602033ca668488c09fb3a2cc3f5
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2024-07-19 08:56:44 +0530

    Time: 7 ms (43.08%), Space: 45.2 MB (47.78%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,37 @@
+class Solution
+{
+    public int[] asteroidCollision(int[] asteroids)
+    {
+        Stack<Integer> stack = new Stack<>();
+        for(int asteroid : asteroids)
+        {
+            boolean alive = true;
+            while (alive && !stack.isEmpty() && stack.peek() > 0 && asteroid < 0)
+            {
+                if (Math.abs(stack.peek()) < Math.abs(asteroid))
+                {
+                    stack.pop();
+                }
+                else if (Math.abs(stack.peek()) == Math.abs(asteroid))
+                {
+                    stack.pop();
+                    alive = false;
+                } 
+                else 
+                {
+                    alive = false;
+                }
+            }
+            if(alive)
+            {
+                stack.push(asteroid);
+            }
+        }
+        int[] result = new int[stack.size()];
+        for (int i = result.length - 1; i >= 0; i--) 
+        {
+            result[i] = stack.pop();
+        }
+        return result;    
+    }
+}
