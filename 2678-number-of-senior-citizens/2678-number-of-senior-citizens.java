56d2972b998958eb5ec00bbef010c770af59a080
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2024-08-01 10:15:44 +0530

    Time: 0 ms (100.00%), Space: 42 MB (94.58%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -5,8 +5,7 @@ class Solution
         int ans = 0;
         for(String detail:details)
         {
-            int age = (detail.charAt(11)-'0')*10+(detail.charAt(12)-'0');
-            if(age>60)
+            if ((detail.charAt(11) - '0') * 10 + (detail.charAt(12) - '0') > 60) 
             {
                 ans++;
             }
