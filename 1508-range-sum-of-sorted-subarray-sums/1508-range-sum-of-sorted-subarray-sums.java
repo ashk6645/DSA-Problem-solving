9bd9f2137cbd5d646654fe682110b863b5640134
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2024-08-04 10:02:27 +0530

    Time: 199 ms (13.06%), Space: 75.2 MB (5.40%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
@@ -1,27 +1,29 @@
 class Solution
 {
+    private static final int MOD = 1_000_000_007;
    
     public int rangeSum(int[] nums, int n, int left, int right) 
     {
-        List<Integer> subSum = new ArrayList<>();
+       PriorityQueue<Long> minHeap = new PriorityQueue<>();
+        long result = 0;
 
-        for (int i = 0; i < n; i++)
-        {
-            int sum = 0;
-            for (int j = i; j < n; j++)
-            {
+        // Compute all subarray sums
+        for (int i = 0; i < n; i++) {
+            long sum = 0;
+            for (int j = i; j < n; j++) {
                 sum += nums[j];
-                subSum.add(sum);
+                minHeap.offer(sum);
             }
         }
-        
-        Collections.sort(subSum);
-        long result = 0;
-        for (int i = left - 1; i < right; i++)
-        {
-            result += subSum.get(i);
+
+        // Extract the sums from left to right
+        for (int i = 1; i <= right; i++) {
+            long currentSum = minHeap.poll();
+            if (i >= left) {
+                result = (result + currentSum) % MOD;
+            }
         }
-        
-        return (int) (result % 1000000007);   
+
+        return (int) result;
     }
 }
