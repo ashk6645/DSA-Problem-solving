f59f930a9007dd029216a0e6a3efccadf1a9e4c8
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2024-08-07 10:47:23 +0530

    Time: 9 ms (42.51%), Space: 42.3 MB (43.73%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -1,46 +1,109 @@
 class Solution
 {
-     private static final String[] LESS_THAN_TWENTY = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    private static final String[] TENS = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    private static final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
-    public String numberToWords(int num)
-    {
-          if(num == 0)
-          {
-              return "Zero";
-          }
-        int i = 0;
-        String words = "";
-        while(num > 0)
-        {
-            if(num % 1000 != 0)
-            {
-                words = helper(num % 1000) + THOUSANDS[i] + " " + words;
-            }
-            num /= 1000;
-            i++;
-        }
-        
-        return words.trim();
-    }
-    
-    private static String helper(int num)
-    {
-        if (num == 0)
-        {
+     public String digit3(List<Integer> d, int start) {
+        if (start >= d.size())
             return "";
-        } 
-        else if (num < 20)
-        {
-            return LESS_THAN_TWENTY[num] + " ";
+        int one = d.get(start);
+        if (start + 1 == d.size())
+            return oneToWord(one);
+
+        int ten = d.get(start + 1);
+        String ret = "";
+
+        if (ten == 1) {
+            ret = tenToNineteen(one);
+        } else {
+            ret = oneToWord(one);
+            if (one != 0 && ten != 0)
+                ret = " " + ret;
+            ret = tenToNinety(ten) + ret;
         }
-        else if (num < 100)
-        {
-            return TENS[num / 10] + " " + helper(num % 10);
+
+        if (start + 2 == d.size() || d.get(start + 2) == 0)
+            return ret;
+
+        if (ten != 0 || one != 0)
+            ret = " " + ret;
+
+        int hundred = d.get(start + 2);
+        ret = "Hundred" + ret;
+
+        ret = oneToWord(hundred) + " " + ret;
+
+        return ret;
+    }
+
+    public String numberToWords(int num) {
+        if (num == 0)
+            return "Zero";
+        List<Integer> digits = new ArrayList<>();
+        while (num != 0) {
+            digits.add(num % 10);
+            num /= 10;
         }
-        else
-        {
-            return LESS_THAN_TWENTY[num / 100] + " Hundred " + helper(num % 100);
+
+        String ret = digit3(digits, 0);
+        String thousand = digit3(digits, 3);
+        String mil = digit3(digits, 6);
+        String bil = digit3(digits, 9);
+
+        if (!ret.isEmpty() && !thousand.isEmpty())
+            ret = " " + ret;
+        if (!thousand.isEmpty())
+            ret = thousand + " Thousand" + ret;
+        if (!ret.isEmpty() && !mil.isEmpty())
+            ret = " " + ret;
+        if (!mil.isEmpty())
+            ret = mil + " Million" + ret;
+        if (!ret.isEmpty() && !bil.isEmpty())
+            ret = " " + ret;
+        if (!bil.isEmpty())
+            ret = bil + " Billion" + ret;
+        return ret.trim();
+    }
+
+    private String oneToWord(int num) {
+        switch (num) {
+            case 1: return "One";
+            case 2: return "Two";
+            case 3: return "Three";
+            case 4: return "Four";
+            case 5: return "Five";
+            case 6: return "Six";
+            case 7: return "Seven";
+            case 8: return "Eight";
+            case 9: return "Nine";
+            default: return "";
+        }
+    }
+
+    private String tenToNineteen(int num) {
+        switch (num) {
+            case 0: return "Ten";
+            case 1: return "Eleven";
+            case 2: return "Twelve";
+            case 3: return "Thirteen";
+            case 4: return "Fourteen";
+            case 5: return "Fifteen";
+            case 6: return "Sixteen";
+            case 7: return "Seventeen";
+            case 8: return "Eighteen";
+            case 9: return "Nineteen";
+            default: return "";
+        }
+    }
+
+    private String tenToNinety(int num) {
+        switch (num) {
+            case 2: return "Twenty";
+            case 3: return "Thirty";
+            case 4: return "Forty";
+            case 5: return "Fifty";
+            case 6: return "Sixty";
+            case 7: return "Seventy";
+            case 8: return "Eighty";
+            case 9: return "Ninety";
+            default: return "";
         }
     }
 }
