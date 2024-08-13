dfaab1a5009c49211e07a4d6bd685d242d9224ac
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2024-08-13 09:30:50 +0530

    Time: 3 ms (82.44%), Space: 43.3 MB (25.35%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -2,33 +2,36 @@ class Solution
 {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) 
     {
-       List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(candidates);
-        backtrack(result, new ArrayList<>(), candidates, target, 0);
+         List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates); 
+        findCombinations(candidates, 0, target, new ArrayDeque<>(), result);
         return result;
     }
-    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] candidates, int remain, int start)
-    {
-        if(remain < 0)
+    
+    private void findCombinations(int[] candidates, int index, int target, Deque<Integer> current, List<List<Integer>> result) {
+        if (target == 0) 
         {
+            result.add(new ArrayList<>(current));
             return;
         }
-        else if(remain == 0)
-        {
-            result.add(new ArrayList<>(tempList));
-        }
-        else
-        {
-            for (int i = start; i < candidates.length; i++) 
+        
+        for (int i = index; i < candidates.length; i++) {
+            if (i > index && candidates[i] == candidates[i - 1])
             {
-                if (i > start && candidates[i] == candidates[i - 1])
-                {
-                    continue; 
-                }
-                tempList.add(candidates[i]); 
-                backtrack(result, tempList, candidates, remain - candidates[i], i + 1); 
-                tempList.remove(tempList.size() - 1); 
+                continue;
             }
-        }
+            
+            int candidate = candidates[i];
+            if (candidate > target)
+            {
+                break;
+            }
+            
+            current.addLast(candidate);
+            findCombinations(candidates, i + 1, target - candidate, current, result);
+            current.removeLast(); // Backtrack by removing the last element
+        } 
+            
+        
     }
 }
