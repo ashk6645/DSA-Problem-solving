3c507a473f85cfb2b75b69c13d945f20ae95d4fb
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2024-08-20 09:10:52 +0530

    Time: 3 ms (89.81%), Space: 42.8 MB (55.10%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.java
+++ 1140-stone-game-ii/1140-stone-game-ii.java
@@ -2,37 +2,59 @@ class Solution
 {
     public int stoneGameII(int[] piles)
     {
-         int n = piles.length;
-        int[][] dp = new int[n][n + 1];
-        for (int[] row : dp) 
-        {
-            Arrays.fill(row, -1);
+        int n = piles.length;
+        int[] totalStonesCache = new int[n + 1];
+        int totalStones = 0;
+        for (int index = n - 1; index >= 0; index--) {
+            totalStones += piles[index];
+            totalStonesCache[index] = totalStones;
         }
-        int[] suffixSum = new int[n];
-        suffixSum[n - 1] = piles[n - 1];
-        for (int i = n - 2; i >= 0; i--) 
-        {
-            suffixSum[i] = piles[i] + suffixSum[i + 1];
+
+        // If the index is n, the answer is 0.
+        int maxM = (n + 2) / 2;
+        Integer[][] memo = new Integer[n + 1][maxM + 1];
+        for (int m = 1; m <= maxM; m++) {
+            memo[n][m] = 0;
         }
-        return helper(0, 1, piles, dp, suffixSum);
+
+        // Solve.
+        return stoneGame(n, 0, 1, totalStonesCache, memo);
     }
-     private int helper(int i, int M, int[] piles, int[][] dp, int[] suffixSum)
-     {
-        if (i == piles.length)
-        {
-            return 0;
+
+    private static int stoneGame(int n, int index, int m, int[] totalStonesCache, Integer[][] memo) {
+        Integer maybeResult = memo[index][m];
+        if (maybeResult != null) {
+            return maybeResult;
         }
-        if (dp[i][M] != -1)
-        {
-            return dp[i][M];
+
+        int result = stoneGameUncached(n, index, m, totalStonesCache, memo);
+        memo[index][m] = result;
+        return result;
+    }
+
+    private static int stoneGameUncached(int n, int index, int m, int[] totalStonesCache, Integer[][] memo) {
+        // Take all the stones if possible.
+        int totalStones = totalStonesCache[index];
+        if (index + 2 * m >= n) {
+            return totalStones;
         }
-        int maxStones = 0;
-        for (int x = 1; x <= 2 * M && i + x <= piles.length; x++)
-        {
-            int opponent = helper(i + x, Math.max(M, x), piles, dp, suffixSum);
-            maxStones = Math.max(maxStones, suffixSum[i] - opponent);
+
+        // Take up to m piles. The next value of m stays the same.
+        int lastIndex = index + m;
+        index++;
+        int maxStones = totalStones - stoneGame(n, index, m, totalStonesCache, memo);
+        for (index++; index <= lastIndex; index++) {
+            int numStones = totalStones - stoneGame(n, index, m, totalStonesCache, memo);
+            maxStones = Math.max(maxStones, numStones);
         }
-        dp[i][M] = maxStones;
+
+       
+        lastIndex += m;
+        for (; index <= lastIndex; index++) {
+            m++;
+            int numStones = totalStones - stoneGame(n, index, m, totalStonesCache, memo);
+            maxStones = Math.max(maxStones, numStones);
+        }
         return maxStones;
     }
 }
