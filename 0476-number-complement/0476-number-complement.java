0e1485978837a9e2290309a67d4ae8968f219b7f
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2024-08-22 08:43:48 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (12.78%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,12 @@
+class Solution
+{
+    public int findComplement(int num) 
+    {
+        int mask = ~0;
+        while((num & mask) != 0)
+        {
+            mask <<= 1;
+        }
+        return ~mask & ~num;
+    }
+}
