430c972040b0c948e82def6b922d060001d83f99
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2024-10-06 15:23:51 +0530

    Time: 1 ms (96.43%), Space: 41.3 MB (93.25%) - LeetHub

---
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.java
@@ -0,0 +1,24 @@
+class Solution 
+{
+    public boolean areSentencesSimilar(String sentence1, String sentence2) 
+    {
+        String[] words1 = sentence1.split(" ");
+        String[] words2 = sentence2.split(" ");
+        
+        int i = 0; 
+        int j = 0; 
+        
+        while (i < words1.length && i < words2.length && words1[i].equals(words2[i])) 
+        {
+            i++;
+        }
+        
+       
+        while (j < words1.length - i && j < words2.length - i && 
+               words1[words1.length - 1 - j].equals(words2[words2.length - 1 - j])) 
+        {
+            j++;
+        }
+        return i + j == Math.min(words1.length, words2.length);
+    }
+}
