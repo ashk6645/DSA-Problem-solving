fa6169592f46466e5f52530bd217a82d4c31ddbb
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2024-10-10 21:09:36 +0530

    Time: 10 ms (76.62%), Space: 54.8 MB (40.55%) - LeetHub

--- 0962-maximum-width-ramp/0962-maximum-width-ramp.java
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.java
@@ -1,25 +1,24 @@
 class Solution
 {
-    public int maxWidthRamp(int[] nums) 
-    {
+    public int maxWidthRamp(int[] nums) {
         int n = nums.length;
-        Integer[] indices = new Integer[n];
-         for (int i = 0; i < n; i++)
-        {
-            indices[i] = i;
-        }
-        Arrays.sort(indices, (i, j) -> nums[i] != nums[j] ? nums[i] - nums[j] : i - j);
-
-        int minIndex = n; 
         int maxWidth = 0;
-
-        for (int i : indices) 
-        {
-            maxWidth = Math.max(maxWidth, i - minIndex);
-            minIndex = Math.min(minIndex, i);
-        }
-
-        return maxWidth;
         
+        // Monotonic stack to keep track of smallest indices
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            if (stack.isEmpty() || nums[stack.peek()] > nums[i]) {
+                stack.push(i);
+            }
+        }
+        
+        
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && nums[stack.peek()] <= nums[i]) {
+                maxWidth = Math.max(maxWidth, i - stack.pop());
+            }
+        }
+        
+        return maxWidth;
     }
 }
