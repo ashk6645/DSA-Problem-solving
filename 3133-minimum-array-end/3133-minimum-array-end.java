c77e0204bea5b56e65c5ab5e10ec884b3ed132cb
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2024-11-09 10:07:15 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (79.81%) - LeetHub

---
+++ 3133-minimum-array-end/3133-minimum-array-end.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long minEnd(int n, int x) {
+        long result = x;
+        long mask;
+        n--;
+        
+        for(mask = 1; n > 0; mask <<= 1)
+        {
+            if((mask & x) == 0)
+            {
+                result |= (n&1)*mask;
+                n >>= 1;
+            }
+        }
+        return result;
+        
+    }
+}
